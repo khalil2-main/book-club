d8f55c3335661fce0c6621a82d3a8a6d4cff2184
Circe-melki <midory798464@gmail.com>
2025-10-19 10:51:38 +0100

    renaming the main file to app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express= require('express');
-
+const {connectToDB, getDB} =require('./utils/db');
+const  routes = require('./routers/index');
 const app = express();
 
+// server port
 const PORT=process.env.PORT|| 3000;
-app.listen(PORT,()=>{
-  console.log(`running on port ${PORT}` )
+let db;
+
+//
+
+//connect to database
+connectToDB((err)=>{
+  if(!err){
+    
+    //intialize server
+    app.listen(PORT,()=>{
+      console.log(`running on port ${PORT}` )
+    })
+    
+    app.use(routes) 
+    db=getDB()
+  }
 })
+
