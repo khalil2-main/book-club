f7eb2d24743054c6faf809f973f4514ca95059c2
Circe-melki <midory798464@gmail.com>
2025-10-24 16:50:53 +0100

    add authotication methodes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
 const express= require('express');
 const db =require('./utils/db.js');
 const  routes = require('./routers/index');
-const passport = require('passport');
+
+const passport = require('./utils/local-strategy.js');
 const app = express();
 
 // server port
@@ -13,13 +14,19 @@ const PORT=process.env.PORT|| 3000;
 (async ()=>{
   try{
     await db.connect();
-    app.use(express.json())
-    app.use(passport.initialize())
-    app.use(routes)
+    app.use(express.json());
+    app.use(passport.initialize());
+    // app.use(passport.session())
+    app.use(routes);
     
     app.get('/',(req,res)=>{
       res.send('<h1> home page</h1>')
     })
+    //authentication
+    app.post('/api/auth', passport.authenticate('local', { session: false }), (req ,res)=>{
+    console.log(req.body)
+    res.end()
+  })
     app.use((req, res)=>{
       res.status(404).json({Message: '404 route not found'});
     })
