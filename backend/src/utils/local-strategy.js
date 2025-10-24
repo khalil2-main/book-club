abd87666fa3cf37a1aae4cfed408e3e31dc3bfc5
Circe-melki <midory798464@gmail.com>
2025-10-24 16:51:47 +0100

    add local stratgy

--- backend/src/utils/local-strategy.js
+++ backend/src/utils/local-strategy.js
@@ -1,16 +1,38 @@
 const passport = require('passport');
 const {Strategy} = require('passport-local');
-const { hashPassword } = require('./helpers');
+const { hashPassword, comparePassword } = require('./helpers');
+const User= require('./../models/user')
 
+// passport.serializeUser((user,done)=>{
+//   console.log('inside deserialize')
+//   console.log(`user /n ${user}`)
+//   done(null,user);
+// })
+
+// passport.deserializeUser(async(id,done)=>{
+//   console.log('inside deserialize')
+//   console.log(`user ID: ${id}`)
+//   try{
+//     const findUser= User.findById(id);
+//     if(!findUser)throw new Error('Eser not found');
+//     done(null,user)
+//   }catch(err){
+//     done(err,null)
+//   }
+// })
 passport.use(
-  new Strategy({usernameField:'email'},(username, password, done)=>{
+  new Strategy({usernameField:'email'},async(username, password, done)=>{
+    console.log({username})
+    console.log({password})
     try{
-      const findUser=mockUser.find((user)=>user.username===username);
+      const findUser=await User.findOne({email:username});
       if (!findUser)throw new Error('Eser not found');
-      if(hashPassword(findUser.password)!==password) throw new Error('Password does\'t match');
+      if(!comparePassword(password,findUser.password)) throw new Error('Password does\'t match'+password+'\n'+findUser.password);
       done(null,findUser);
     }catch(err){
       done(err,null);
     }
   })
 )
+
+module.exports=passport
