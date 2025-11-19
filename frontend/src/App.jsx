ea65551889424548831e7acd3303c556783a6f6b
Circe-melki <midory798464@gmail.com>
2025-11-19 23:04:28 +0100

    profile mengement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage"
 import Signup from "./pages/signup";
 import Login from "./pages/login";
+import UserProfile from "./components/UserProfile";
 
 const App = () => {
   return (
@@ -11,6 +12,8 @@ const App = () => {
         <Route path="" element={<HomePage/> } ></Route>     
         <Route path="signup" element={<Signup/> } ></Route>
         <Route path="login" element={<Login/> } ></Route>
+        <Route path="profile" element={<UserProfile/>}></Route>
+        <Route path="*" element={<h1>404 Not Found</h1>} ></Route>
       </Routes>
     </>
   );
