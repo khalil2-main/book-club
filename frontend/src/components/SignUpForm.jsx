43612b024ec6dfa65bef0f5aee155164e4d6c8fe
Circe-melki <midory798464@gmail.com>
2025-11-22 15:09:18 +0100

    dix some validator issues

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -31,6 +31,8 @@ const SignUpForm = () => {
       case "firstname":
       case "lastname":
         if (!value.trim()) return "This field is required";
+        if (!letterRegex.test(value)) return " must be letters only";
+        
         if (value.length < 2) return "Must be at least 2 characters";
         return "";
 
@@ -44,17 +46,12 @@ const SignUpForm = () => {
         return "";
 
       case "location":
-        if (!value) return "Address is required";
+      
         return "";
 
       case "city":
-        if (!value) return "City is required";
-        if (!letterRegex.test(value)) return "City must be letters only";
-        return "";
-
       case "country":
-        if (!value) return "Country is required";
-        if (!letterRegex.test(value)) return "Country must be letters only";
+        if (value && !letterRegex.test(value)) return "Letters only";
         return "";
 
       case "password":
