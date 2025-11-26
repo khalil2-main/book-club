519a91dfad53c588dffdbb15d64268c746301e6f
Circe-melki <midory798464@gmail.com>
2025-11-26 14:11:10 +0100

    add admin rout protection

---
+++ frontend/src/components/AdminRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router";
+import { useAuth } from "../context/AuthContext";
+
+const AdminRoute = ({ children }) => {
+  const { auth, admin, loading } = useAuth();
+
+  if (loading) return <div>Loading...</div>;
+
+  if (!auth) return <Navigate to="/login" replace />;
+
+  if (!admin) return <Navigate to="/" replace />;
+
+  return children;
+};
+
+export default AdminRoute;
