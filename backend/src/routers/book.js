b0e7043e83f776d6a696e32fb8abd4f2f51fca0a
Circe-melki <midory798464@gmail.com>
2025-12-04 01:13:00 +0100

    create books CRUD

--- backend/src/routers/book.js
+++ backend/src/routers/book.js
@@ -1,39 +1,61 @@
 const { Router}= require('express')
+const Book = require('../models/bookModel')
+const {validationResult, matchedData}=require('express-validator')
 
 const router=Router();
 
 
 
-// Liste statique de livres
-const staticBooks = [
-  {
-    _id: '1',
-    title: 'Le Petit Prince',
-    author: 'Antoine de Saint-Exupéry',
-    description: 'Un conte poétique et philosophique.',
-    createdAt: new Date('2025-11-01')
-  },
-  {
-    _id: '2',
-    title: 'L’Étranger',
-    author: 'Albert Camus',
-    description: 'Un roman sur l’absurdité de la vie.',
-    createdAt: new Date('2025-11-10')
-  },
-  {
-    _id: '3',
-    title: '1984',
-    author: 'George Orwell',
-    description: 'Un classique de la dystopie politique.',
-    createdAt: new Date('2025-11-20')
-  }
-];
 
-router.get('/', (req, res) => {
-  // Option de tri par date de création (du plus récent au plus ancien)
-  const sorted = [...staticBooks].sort((a, b) => b.createdAt - a.createdAt);
-  res.json(sorted);
+// show the top 15 puppolair book
+router.get('/', async(req, res) => {
+  try{
+    const books= await Book.find().sort({rating:1}).limit(15); 
+    res.status(200).send({books})
+  }
+  catch(err){
+    console.log(err);
+    return res.status(500).send('BAD REQEST')
+  }
+  
 });
+
+// 
+router.get('/', async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = 20;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ rating: 1 })
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).send({ page, books });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send('BAD REQUEST');
+  }
+});
+
+router.post('/', bookValidator, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: "Validation failed",details: errors.array()
+    });
+  }
+  const data = matchedData(req);
+  try {
+    const book = new Book(data);
+    const savedBook = await book.save();
+    res.status(201).json({ message: "Book created", book: savedBook });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "BAD REQEST" });
+  }
+});
+
 
 
 
