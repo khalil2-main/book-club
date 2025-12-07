c0e1c5626ffb5b7fff6c4f70aca8e8004c205da2
Circe-melki <midory798464@gmail.com>
2025-12-07 16:58:36 +0100

    rerate the validation schema to use body valdation

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt= require('jsonwebtoken')
-const User= require('./../models/user')
+const User= require('../models/userModel')
 
 const requireAuth= (req,res,next)=>{
   const token=req.cookies.jwt;
--- backend/src/validators/UservalidationSchema.js
+++ backend/src/validators/UservalidationSchema.js
@@ -1,175 +1,111 @@
-const { options } = require("../routers");
+const { body } = require("express-validator");
 
+// Create user validation
+const createUserValidator = [
+  body("firstname")
+    .isString().withMessage("firstname must be a string")
+    .isLength({ min: 3 }).withMessage("firstname must be at least 3 characters")
+    .notEmpty().withMessage("firstname is required"),
 
-const createUserValidatorsScheama={
-  firstname:{
-    isLength:{
-      options:{
-        min:3
-      },
-      errorMessage:'firstname should at least contains 3 characterss'
-    },
-    notEmpty:{
-      errorMessage:'fill the first,ame field'
-    }
-  },
-  lastname:{
-    isLength:{
-      options:{
-        min:3,
-        
-      },
-      errorMessage:'lastname should at least contains 3 characterss'
-    },
-    notEmpty:{
-      errorMessage:'fill the lastname field'
-    }
-  },
-   admin:{
-    optional:true,
-    isBoolean:{
-      errorMessage:'invalid admin data'
-    }
-  },
- 
-  email:{
-    isEmail:{
-      
-      errorMessage:'invalid email foramat'
-    },
-    notEmpty:{
-      errorMessage:'Email is required'
-    }
-  },
-  birthday:{
-    isISO8601:{
-      
-      errorMessage:'invalid date foramat'
-    },
-    notEmpty:{
-      errorMessage:'birthday is required'
-    }
-  },
-  password:{
-    isLength:{
-      options:{
-        min:8,
-        max:30
-        
-      },
-      errorMessage:'password should at least contains 8 characterss'
-    },
-    notEmpty:{
-      errorMessage:'password is required'
-    }
-  },
-  'address.location': {
-    optional: true,
-    isString: { errorMessage: 'address.location must be a string' }
-  },
-  'address.city': {
-    optional: true,
-    isString: { errorMessage: 'address.city must be a string' }
-  },
-  'address.country': {
-    optional: true,
-    isString: { errorMessage: 'address.country must be a string' }
-  }
-}
-const updateUserValidatorsScheama={
-  firstname:{
-    optional: true,
-    isLength:{
-      options:{
-        min:3
-      },
-      errorMessage:'firstname should at least contains 3 characterss'
-    }
-  },
-  lastname:{
-    optional: true,
-    isLength:{
-      options:{
-        min:3,
-        
-      },
-      errorMessage:'lastname should at least contains 3 characterss'
-    }
-  },
- 
-  email:{
-    optional: true,
-    isEmail:{
-      
-      errorMessage:'invalid email foramat'
-    }
-  },
-  birthday:{
-    optional: true,
-    isISO8601:{
-      
-      errorMessage:'invalid date foramat'
-    }
-  },
-  password:{
-    optional: true,
-    isLength:{
-      options:{
-        min:8,
-        max:30
-        
-      },
-      errorMessage:'password should at least contains 8 characterss'
-    }
-  },
-  admin:{
-    optional:true,
-    isBoolean:{
-      errorMessage:'invalid admin data'
-    }
-  },
+  body("lastname")
+    .isString().withMessage("lastname must be a string")
+    .isLength({ min: 3 }).withMessage("lastname must be at least 3 characters")
+    .notEmpty().withMessage("lastname is required"),
 
-   'address.location': {
-    optional: true,
-    isString: { errorMessage: 'address.location must be a string' }
-  },
-  'address.city': {
-    optional: true,
-    isString: { errorMessage: 'address.city must be a string' }
-  },
-  'address.country': {
-    optional: true,
-    isString: { errorMessage: 'address.country must be a string' }
-  }
-}
+  body("email")
+    .isEmail().withMessage("invalid email format")
+    .notEmpty().withMessage("email is required"),
 
+  body("birthday")
+    .isISO8601().withMessage("invalid date format")
+    .notEmpty().withMessage("birthday is required"),
 
-//auth
-const AuthUserValidatorsScheama={ 
-    email:{
-    isEmail:{
-      
-      errorMessage:'invalid email foramat'
-    },
-    notEmpty:{
-      errorMessage:'Email is required'
-    }
-  },
-  
-  password:{
-    isLength:{
-      options:{
-        min:8,
-        max:30
-        
-      },
-      errorMessage:'password should at least contains 8 characterss'
-    },
-    notEmpty:{
-      errorMessage:'password is required'
-    }
-  },
+  body("password")
+    .isLength({ min: 8, max: 30 }).withMessage("password must be 8–30 characters")
+    .notEmpty().withMessage("password is required"),
 
-}
+  body("admin")
+    .optional()
+    .isBoolean().withMessage("admin must be a boolean"),
 
-module.exports= {createUserValidatorsScheama,updateUserValidatorsScheama, AuthUserValidatorsScheama}
+  body("address.location")
+    .optional()
+    .isString().withMessage("address.location must be a string"),
+
+  body("address.city")
+    .optional()
+    .isString().withMessage("address.city must be a string"),
+
+  body("address.country")
+    .optional()
+    .isString().withMessage("address.country must be a string"),
+];
+
+// Update user validation
+const updateUserValidator = [
+  body("firstname")
+    .optional()
+    .isString().withMessage("firstname must be a string")
+    .isLength({ min: 3 }).withMessage("firstname must be at least 3 characters"),
+
+  body("lastname")
+    .optional()
+    .isString().withMessage("lastname must be a string")
+    .isLength({ min: 3 }).withMessage("lastname must be at least 3 characters"),
+
+  body("email")
+    .optional()
+    .isEmail().withMessage("invalid email format"),
+
+  body("birthday")
+    .optional()
+    .isISO8601().withMessage("invalid date format"),
+
+  body("password")
+    .optional()
+    .isLength({ min: 8, max: 30 }).withMessage("password must be 8–30 characters"),
+
+  body("admin")
+    .optional()
+    .isBoolean().withMessage("admin must be a boolean"),
+
+  body("address.location")
+    .optional()
+    .isString().withMessage("address.location must be a string"),
+
+  body("address.city")
+    .optional()
+    .isString().withMessage("address.city must be a string"),
+
+  body("address.country")
+    .optional()
+    .isString().withMessage("address.country must be a string"),
+];
+
+// Auth validator
+const authUserValidator = [
+  body("email")
+    .isEmail().withMessage("invalid email format")
+    .notEmpty().withMessage("email is required"),
+
+  body("password")
+    .isLength({ min: 8, max: 30 }).withMessage("password must be 8–30 characters")
+    .notEmpty().withMessage("password is required"),
+];
+
+const uploadProfileImageValidator = [
+  body("profileImage")
+    .optional()
+    .matches(/^image\/(jpeg|jpg|png|gif|webp)$/i)
+    .withMessage("Invalid image type"),
+
+  body("data")
+    .notEmpty().withMessage("Image buffer is required")
+  ];
+
+module.exports = {
+  createUserValidator,
+  updateUserValidator,
+  authUserValidator,
+  uploadProfileImageValidator
+};
