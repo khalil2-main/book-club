f8246b4ce7e602713e8adf43f0a8c60bb5957e25
Circe-melki <midory798464@gmail.com>
2025-12-08 19:28:42 +0100

    make the upload middleware flexible  for storing in diffrent folders

--- backend/src/middlewares/upload.js
+++ backend/src/middlewares/upload.js
@@ -2,34 +2,36 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-// Ensure upload directory exists
-const uploadDir = path.join(__dirname, '../../uploads/users');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
+const creatUploader= (folderName)=>{
+      // Ensure upload directory exists
+    const uploadDir = path.join(__dirname, `../../uploads/${folderName}`);
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+
+    //MULTER STORAGE
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, uploadDir);
+      },
+      filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname); 
+        cb(null, `${Date.now()}-${file.fieldname}${ext}`); 
+      },
+    });
+
+    //File filter
+    const fileFilter = (req, file, cb) => {
+      // Only accept images
+      if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+      } else {
+        cb(new Error('Only image files are allowed'), false);
+      }
+    };
+
+    // ---------------- EXPORT UPLOAD ----------------
+    return multer({ storage, fileFilter });
 }
 
-//MULTER STORAGE
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname); 
-    cb(null, `${Date.now()}-${file.fieldname}${ext}`); 
-  },
-});
-
-//File filter
-const fileFilter = (req, file, cb) => {
-  // Only accept images
-  if (file.mimetype.startsWith('image/')) {
-    cb(null, true);
-  } else {
-    cb(new Error('Only image files are allowed'), false);
-  }
-};
-
-// ---------------- EXPORT UPLOAD ----------------
-const upload = multer({ storage, fileFilter });
-
-module.exports = upload;
+module.exports = creatUploader;
