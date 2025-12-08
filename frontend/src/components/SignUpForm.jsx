61e37917fa1f97ddf1fc7d6c26362a0f714cd0b8
Circe-melki <midory798464@gmail.com>
2025-12-08 23:14:06 +0100

    switch to react router dom

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Input from "../components/Input";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../context/AuthContext";
 
