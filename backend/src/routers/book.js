2e36f90c109426f252684a8214c7c6dcc6bfc620
Circe-melki <midory798464@gmail.com>
2025-12-09 08:51:25 +0100

    rebuild the post rquest

--- backend/src/routers/book.js
+++ backend/src/routers/book.js
@@ -3,6 +3,7 @@ const Book = require('./../models/bookModel')
 const {validationResult, matchedData,}=require('express-validator');
 const {bookCreationValidator,bookUpdateValidator, isParamValidator} =require ('../validators/books-validator-schema')
 const creatUploader= require('../middlewares/upload')
+const validate= require('../middlewares/validate')
 const router=Router();
 const fs = require('fs');
 const path = require('path');
@@ -43,27 +44,30 @@ router.get('/', async (req, res) => {
 
 //Create a book
 
-router.post('/',upload.single('image'), bookCreationValidator , async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ error: "Validation failed",details: errors.array()
-    });
-  }
-  const data = matchedData(req);
-  try {
-    const book = new Book(data);
+router.post('/',upload.single('image'),bookCreationValidator,validate,async (req, res) => {
 
-    if(req.file){
-      
-     data.coverImageUrl= `/uploads/books/${req.file.filename}`
+    let data = matchedData(req);
+
+    try {
+      if (req.file) {
+        data.coverImageUrl = `/uploads/books/${req.file.filename}`;
+      }
+
+      const book = new Book(data);
+      const savedBook = await book.save();
+
+      res.status(201).json({
+        message: "Book created",
+        book: savedBook
+      });
+
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "BAD REQUEST" });
     }
-    const savedBook = await book.save();
-    res.status(201).json({ message: "Book created", book: savedBook });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "BAD REQEST" });
-  }
 });
+
+
 router.patch('/:id',isParamValidator, bookUpdateValidator, async (req, res) => {
   const {id}= req.params
   const errors = validationResult(req);
