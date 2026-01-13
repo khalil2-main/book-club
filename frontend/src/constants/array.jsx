2a714f1624b979f9ab3410362b89eecfa86d1a56
Circe-melki <midory798464@gmail.com>
2026-01-13 14:21:27 +0100

    place the array in their own coponent

---
+++ frontend/src/constants/array.jsx
@@ -0,0 +1,39 @@
+  const genreOptions = [
+    "Fantasy","High Fantasy","Urban Fantasy","Science Fiction","Cyberpunk","Dystopian","Military Fiction",
+    "Post-Apocalyptic","Space Opera","Romance","Historical Romance","Contemporary Romance",
+    "Thriller","Mystery","Crime","Detective","Psychological Thriller","Horror",
+    "Gothic Horror","Paranormal","Adventure","Action","Drama","Literary Fiction",
+    "Classics","Young Adult","New Adult","Children's Literature","Fairy Tales",
+    "Mythology","Magical Realism","Humor","Satire","Short Stories","Western","War Fiction",
+    "Military Science Fiction","Biography","Autobiography","Memoir","Self-Help",'Fiction',
+    "Personal Development","Philosophy","Psychology","Sociology","Anthropology","History",
+    "Military History","World History","Politics","Economics","Business","Finance",
+    "Entrepreneurship","Marketing","Leadership","Education","Parenting","Religion","Magic",
+    "Spirituality","True Crime","Travel","Cookbooks","Food & Nutrition","Health & Wellness",
+    "Fitness","Art","Photography","Music","Film Studies","Writing","Linguistics","Enemy to lovers",
+    "Poetry","Essays","Journalism","Crafts & DIY","Science","Biology","Ecology", "Friends to lovers",
+    "Evolution","Chemistry","Physics","Astronomy","Earth Science","Geology","Neuroscience",
+    "Mathematics","Statistics","Engineering","Computer Science","Information Technology",
+    "Software Engineering","Programming","Web Development","Backend Development",
+    "Frontend Development","Mobile Development","Artificial Intelligence","Machine Learning",
+    "Data Science","Cybersecurity","Cloud Computing","DevOps","Networking","Databases",
+    "Algorithms","Operating Systems","Law","Medicine","Nursing","Public Health",
+    "Architecture","Design",
+    "Children","Young adult", "Adult"
+  ].sort();
+  const languages = [
+  "Abkhaz","Afar","Afrikaans","Akan","Albanian","Amharic","Arabic","Aragonese","Armenian","Assamese","Avaric","Avestan","Aymara","Azerbaijani","Bambara","Bashkir","Basque","Belarusian","Bengali","Bihari","Bislama","Bosnian","Breton","Bulgarian","Burmese","Catalan","Chamorro","Chechen","Chichewa","Chinese",
+
+  "Church Slavic","Chuvash","Cornish","Corsican","Cree","Croatian","Czech","Danish","Divehi","Dutch","Dzongkha","English","Esperanto","Estonian","Ewe","Faroese","Fijian","Finnish","French","Fulah","Galician","Georgian","German","Greek","Guaraní","Gujarati","Haitian","Hausa","Hebrew","Herero",
+
+  "Hindi","Hiri Motu","Hungarian","Icelandic","Ido","Igbo","Indonesian","Interlingua","Interlingue","Inuktitut","Inupiaq","Irish","Italian","Japanese","Javanese","Kalaallisut","Kannada","Kanuri","Kashmiri","Kazakh","Khmer","Kikuyu","Kinyarwanda","Kirghiz","Komi","Korean","Kuanyama","Kurdish","Lao","Latin",
+
+  "Latvian","Limburgish","Lingala","Lithuanian","Luba-Katanga","Luxembourgish","Macedonian","Malagasy","Malay","Malayalam","Maltese","Manx","Maori","Marathi","Marshallese","Mongolian","Nauru","Navajo","Ndonga","Nepali","Northern Sami","Norwegian","Norwegian Bokmål","Norwegian Nynorsk","Occitan","Ojibwa","Oriya","Oromo","Ossetian","Pali",
+
+  "Pashto","Persian","Polish","Portuguese","Punjabi","Quechua","Romanian","Romansh","Russian","Samoan","Sango","Sanskrit","Sardinian","Scottish Gaelic","Serbian","Shona","Sindhi","Sinhala","Slovak","Slovenian","Somali","Southern Sotho","Spanish","Sundanese","Swahili","Swati","Swedish","Tagalog","Tahitian","Tajik",
+
+  "Tamil","Tatar","Telugu","Thai","Tibetan","Tigrinya","Tonga","Tsonga","Tswana","Turkish","Turkmen","Twi","Uighur","Ukrainian","Urdu","Uzbek","Venda","Vietnamese","Volapük","Walloon","Welsh","Western Frisian","Wolof","Xhosa","Yiddish","Yoruba","Zhuang","Zulu"
+].sort();
+
+
+export { genreOptions, languages };
